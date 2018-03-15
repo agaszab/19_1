40bc428bf3191f3ea2992ec181cbfdc89000634e
agaszab <gatito@poczta.pl>
2018-03-15 15:06:04 +0100

    zadania

--- src/test/java/pl/javastart/FibonacciTest.java
+++ src/test/java/pl/javastart/FibonacciTest.java
@@ -32,4 +32,22 @@ public class FibonacciTest {
 
         Assert.assertThat(wynik, CoreMatchers.is(0));
     }
+
+    @Test
+    public void shouldSumFor15() {
+        Fibonacci fb = new Fibonacci(0);
+
+        int wynik = fb.suma(15);
+
+        Assert.assertThat(wynik, CoreMatchers.is(798));
+    }
+
+    @Test
+    public void shouldSumFor21() {
+        Fibonacci fb = new Fibonacci(21);
+
+        int wynik = fb.suma(21);
+
+        Assert.assertThat(wynik, CoreMatchers.is(14328));
+    }
 }
